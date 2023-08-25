d679d8a4e1492911b83301be59ca200a8ae00b7e
Ankit-Kumar4 <113576375+Ankit-Kumar4@users.noreply.github.com>
2023-08-25 21:08:13 +0530

    Add project files.

---
+++ Employee/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee
+{
+    internal class Program
+    {
+
+
+        public int age { get; set; }
+        public double sal { get; set; }
+        public String name { get; set; }
+
+
+        public Program( string name,int age,double sal)
+        {
+            name = name;
+            age = age;
+            sal = sal;
+        }
+        public void Display()
+
+        {
+            Console.WriteLine($"Name:{name}");
+            Console.WriteLine($"Age:{age}");
+            Console.WriteLine($"SAl:{sal}");
+
+        }
+
+
+
+
+    }
+    class P
+    {
+        static void Main(string[] args)
+        {
+            Program obj = new Program("john doe", 45, 5000);
+            obj.Display();
+        }
+    }
+
+}
+
